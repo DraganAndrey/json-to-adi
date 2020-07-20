bd9c73c903136408a4f7929586327741b597d396
Aleksandr Lezhachev <alexander.lezhachev@stm-labs.ru>
2020-07-20 20:43:27 +0300

    Добавил выгрузку xml

--- src/main/java/ru/stm/JsonToADIConverter/JsonToAdiConverterApplication.java
+++ src/main/java/ru/stm/JsonToADIConverter/JsonToAdiConverterApplication.java
@@ -1,12 +1,44 @@
 package ru.stm.JsonToADIConverter;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import ru.stm.JsonToADIConverter.pojo.InputJson;
+import ru.stm.JsonToADIConverter.service.ConvertService;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
 @SpringBootApplication
-public class JsonToAdiConverterApplication {
+public class JsonToAdiConverterApplication implements CommandLineRunner {
+
+	@Autowired
+	ConvertService convertService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(JsonToAdiConverterApplication.class, args);
+	}
+
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		File file = new File("/home/lezhachev/Загрузки/movies.txt");
+		ObjectMapper objectMapper = new ObjectMapper();
+		StringBuilder builder = new StringBuilder();
+		try {
+			InputJson json1 = objectMapper.readValue(file, InputJson.class);
+			convertService.convert(json1);
+
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
--- src/main/java/ru/stm/JsonToADIConverter/service/impl/ConvertServiceImpl.java
+++ src/main/java/ru/stm/JsonToADIConverter/service/impl/ConvertServiceImpl.java
@@ -8,6 +8,11 @@ import ru.stm.JsonToADIConverter.schema.*;
 import ru.stm.JsonToADIConverter.service.AssetService;
 import ru.stm.JsonToADIConverter.service.ConvertService;
 
+import javax.xml.bind.JAXBElement;
+import javax.xml.namespace.QName;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -26,22 +31,39 @@ public class ConvertServiceImpl implements ConvertService {
     public void convert(InputJson inputJson) {
 
 
-        if(Objects.isNull(inputJson.getResult()) || Objects.isNull(inputJson.getResult().getItems())){
+        if (Objects.isNull(inputJson.getResult()) || Objects.isNull(inputJson.getResult().getItems())) {
             log.warn("Не найдены MovieItem во входящем Json");
             return;
         }
-        List<ADIType> adiTypeList = inputJson.getResult().getItems().stream().map(item-> {
+        List<ADIType> adiTypeList = inputJson.getResult().getItems().stream().map(item -> {
             List<AssetType> assetTypeList = assetServiceList
                     .stream()
-                    .map(service-> service.prepareAsset(item))
+                    .map(service -> service.prepareAsset(item))
                     .collect(Collectors.toList());
             ADIType adiType = new ADIType();
             adiType.setAsset(assetTypeList.stream().filter(asset -> asset.getMetadata().getAMS().getAssetClass().equals("package")).findFirst().get());
             adiType.getAsset()
                     .getAsset()
-                    .addAll(assetTypeList.stream().filter(asset-> !asset.getMetadata().getAMS().getAssetClass().equals("package")).collect(Collectors.toList()));
+                    .addAll(assetTypeList.stream().filter(asset -> !asset.getMetadata().getAMS().getAssetClass().equals("package")).collect(Collectors.toList()));
             return adiType;
         }).collect(Collectors.toList());
+        adiTypeList.forEach(adiType -> {
+            try {
+                javax.xml.bind.JAXBContext jaxbCtx = javax.xml.bind.JAXBContext.newInstance(adiType.getClass().getPackage().getName());
+                javax.xml.bind.Marshaller marshaller = jaxbCtx.createMarshaller();
+                marshaller.setProperty(javax.xml.bind.Marshaller.JAXB_ENCODING, "UTF-8");
+                marshaller.setProperty(javax.xml.bind.Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+                OutputStream os = new FileOutputStream(String.format("%s_%s.xml", String.valueOf(System.currentTimeMillis()), adiType.getAsset().getMetadata().getAMS().getAssetName()));
+                marshaller.marshal(new JAXBElement<>(new QName("ADIType", "ADI"), ADIType.class, adiType), os);
+
+            } catch (javax.xml.bind.JAXBException ex) {
+                log.error("Ошибка генерации XML {}", ex);
+            } catch (FileNotFoundException e) {
+                log.error("Файл не найден", e);
+            }
+        });
+
+        System.out.println("");
     }
 
 }
